4c41b0d195b43dda72f971454f6f433d73efca1c
foo <foo@email.com>
2014-09-04 20:43:47 -0300

    function renderTasks()

--- practice/mysqli.php
+++ practice/mysqli.php
@@ -1,10 +1,32 @@
 <?php
 
 $mysqli = mysqli_connect("localhost", "hito", "admin", "users_tasks");
-$res = mysqli_query($mysqli, "SELECT * FROM  `users` WHERE name = 'alex'");
-$row = mysqli_fetch_assoc($res);
 
-while (!is_null($row)){
-    echo '<pre>' . var_dump($row) . '</pre>';
-    $row = mysqli_fetch_assoc($res);
+/**
+ * Renderiza todas las tareas de un usuario
+ * @global object $mysqli
+ * @param string $name
+ * @return 
+ */
+function renderTasks($name)
+{
+    global $mysqli;
+    $query_id = mysqli_query($mysqli, "SELECT user_id FROM users WHERE name = '$name'");
+    $rowId = mysqli_fetch_assoc($query_id);
+    if (is_null($rowId)){
+        echo "error hiciste todo mal";
+    } else {
+        $id = $rowId['user_id'];
+        $query_tasks = mysqli_query($mysqli, "SELECT * FROM tasks WHERE user_id = $id");
+        $rowTasks = mysqli_fetch_assoc($query_tasks);
+        echo "$name's tasks: <br/>";
+        echo "<ol>";
+        while (!is_null($rowTasks)){
+            echo "<li>" . $rowTasks['name'] . "</li>";
+            $rowTasks = mysqli_fetch_assoc($query_tasks);
+        };
+        echo "</ol>";
+    };
 }
+
+renderTasks('luigi');
