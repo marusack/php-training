d6c5957f9925e2dd54027309686e2ce6c074e873
Soto <Soto@Soto-PC>
2014-10-08 00:39:25 -0300

    Tablet

---
+++ practice/oop/Tablet/App.php
@@ -0,0 +1,56 @@
+<?php
+
+//namespace Tablet\App;
+
+class App
+{
+    private $name;
+    private $batCons;
+    
+    /**
+     * App constructor
+     * @param string $name
+     * @param int $batCons
+     */
+    public function __construct($name, $batCons)
+    {
+        $this->name = $name;
+        $this->batCons = $batCons;
+    }
+    
+    /**
+     * Set app name
+     * @param string $name
+     */
+    protected function setName($name)
+    {
+        $this->name = $name;
+    }
+    
+    /**
+     * Set battery consumption
+     * @param int $batCons
+     */
+    protected function setBatCons($batCons)
+    {
+        $this->batCons = $batCons;
+    }
+    
+    /**
+     * Get app name
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    /**
+     * Get battery consumption
+     * @return int
+     */
+    public function getBatCons()
+    {
+        return $this->batCons;
+    }
+}
--- practice/oop/Tablet/main.php
+++ practice/oop/Tablet/main.php
@@ -1,4 +1,27 @@
 <?php
-require_once '../bootstrap.php';
 
-use Tablet\Tablet;
+//require_once '../bootstrap.php';
+//
+//use Tablet\Tablet;
+//use Tablet\App;
+
+require_once 'App.php';
+require_once 'Tablet.php';
+
+$a1= new App('Chrome', 1);
+$a2 = new App('Netbeans', 3);
+$a3 = new App('Winamp', 2);
+
+$tablet = new Tablet();
+
+$tablet->installApp($a3);
+$tablet->showInstalledApps();
+echo "</br>";
+
+$tablet->installApp($a2);
+$tablet->showInstalledApps();
+echo "</br>";
+
+$tablet->installApp($a3);
+$tablet->showInstalledApps();
+echo "</br>";
