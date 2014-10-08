ce570efe887771667d1953cfcabfe6fc2c8a4a4f
Soto <Soto@Soto-PC>
2014-10-07 22:48:54 -0300

    Tablet

--- practice/oop/Tablet/tablet.php
+++ practice/oop/Tablet/tablet.php
@@ -1,7 +1,61 @@
 <?php
-namespace Tablet;
+
+namespace Tablet\Tablet;
 
 class Tablet 
 {
+    private $battery;
+    private $apps;
+    private $running;
     
+    /**
+     * Tablet constructor
+     */
+    public function __construct() {
+        $this->battery = 100;
+        $this->apps = array();
+        $this->running = array(false);
+    }
+    
+    /**
+     * Set battery status
+     * @param int $num
+     */
+    private function setBattery($num)
+    {
+        $this->battery = $num;
+    }
+    
+    /**
+     * Get battery status
+     * @return int
+     */
+    public function getBattery() {
+        return $this->battery;
+    }
+    
+    /**
+     * Get installed apps
+     * @return array
+     */
+    public function getApps() {
+        return $this->apps;
+    }
+    
+    /**
+     * Get running
+     * @return boolean
+     */
+    public function getRunning() {
+        return $this->running[0];
+    }
+    
+    public function installApp(App $app)
+    {
+        if (count($this->apps) < 2) {
+            $this->apps[] = $app;
+        } else {
+            echo "Error";
+        }
+    }
 }
