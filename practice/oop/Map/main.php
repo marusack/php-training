64077c12be29aeb3f60708b758404b0041f049f5
foo <foo@email.com>
2014-10-15 09:23:01 -0300

    class 15/10 (hacer lo de ajax)

--- practice/oop/Map/main.php
+++ practice/oop/Map/main.php
@@ -5,5 +5,17 @@ require_once '../bootstrap.php';
 use Map\Province;
 use Map\City;
 
-$pro = new Province('Corrientes');
-echo $pro->getName();
+$ctes = new Province('Corrientes');
+$ctes->addCity(new City('Corrientes', 3400));
+$ctes->addCity(new City('Goya', 3400));
+$ctes->addCity(new City('Paso de la Patria', 3400));
+$ctes->addCity(new City('Empedrado', 3400));
+$ctes->addCity(new City('Mercedes', 3400));
+
+$chaco = new Province('Chaco');
+$chaco->addCity(new City('Resistencia', 3500));
+$chaco->addCity(new City('Barranqueras', 3500));
+$chaco->addCity(new City('Villa Angela', 3500));
+$chaco->addCity(new City('San Martin', 3500));
+$chaco->addCity(new City('Castelli', 3500));
+
