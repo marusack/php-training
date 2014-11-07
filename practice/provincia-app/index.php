4c17c4438ef2d031ebaf267da10abb87d2c19e31
marusack <marielsack@gmail.com>
2014-11-07 18:47:11 -0300

    search+css

---
+++ practice/provincia-app/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Province App - Informatorio 2014</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <!-- <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script> -->
+    </head>
+    <body>
+        <div class="container">
+            <div class="searchbox">
+                <input type="text" name="search" placeholder="Search..." size="40">
+                <input type="button" value="Search">
+            </div>
+            <div class="content">
+               aca se arma la linea de arriba con el top de la caja 
+            </div>
+        </div>  
+    </body>
+</html>
+
